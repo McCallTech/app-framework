81ca36c2a85f4a533f56ec4405daa4ec47ec2598
JoshMcCall <joshmccall221@gmail.com>
2018-05-03 23:49:39 -0500

    app with video

--- demo/pages/home.vue
+++ demo/pages/home.vue
@@ -12,47 +12,7 @@
     <f7-block inner inset style="text-align: center" v-if="$root.appMode==='mobile'">
       <p><b>For native App feeling, please pin this page to your homescreen and open it from there!</b></p>
     </f7-block>
-
-    <!-- Configuration -->
-    <f7-block-title>Style Configuration</f7-block-title>
-    <f7-list>
-      <f7-list-item smart-select smart-select-back-on-select
-        title="Select Theme"
-        :media="'<img src=\'' + images['theme_' + $root.theme] + '\' width=\'29\' />'">
-        <select v-model="$root.theme">
-          <option value="ios" :data-option-image="images.theme_ios">iOS</option>
-          <option value="material" :data-option-image="images.theme_material">Material</option>
-        </select>
-      </f7-list-item>
-      <f7-list-item title="Select Color" :link="'/f7' + $root.theme + '/color-themes/'" media="<i class='f7-icons'>keyboard_fill</i>" />
-      <f7-list-item title="Show Statusbar" media="<i class='f7-icons'>delete</i>" v-if="$root.appMode==='native'">
-        <div slot="after">
-          <label class="label-switch">
-            <input type='checkbox' :checked="$root.statusbarVisibility" @change="$root.statusbarVisibility=$event.target.checked">
-            <div class='checkbox'></div>
-          </label>
-        </div>
-      </f7-list-item>
-      <f7-list-item title="Change text color" @click="$root.statusbarTextColor=$root.statusbarTextColor==='white'?'black':'white'" v-if="$root.$f7.device.ios && $root.appMode === 'native' && $root.statusbarVisibility" link="#" media="<i class='f7-icons'>delete</i>" />
-    </f7-list>
-
-    <!-- Demonstration -->
-    <f7-block-title>Feature Demonstration</f7-block-title>
-    <f7-list>
-      <f7-list-item link="/f7ios/index/" title="iOS Components" media="<i class='icon icon-f7' />" v-if="$root.theme === 'ios'" />
-      <f7-list-item link="/f7material/index/" title="Material Components" media="<i class='icon icon-f7' />" v-if="$root.theme === 'material'" />
-      <f7-list-item link="/firebase-public/" title="Realtime Database" :media="'<img src=\'' + images.firebase + '\' width=\'29\' />'" />
-      <f7-list-item link="/firebase-private/" title="Authentication & Storage" :media="'<img src=\'' + images.firebase + '\' width=\'29\' />'" />
-      <f7-list-item link="/state-restoration/" title="App State Restoration" media="<i class='f7-icons'>refresh</i>" />
-      <f7-list-item link="/flexible-routing/blog/123/post/456/?display=summary#gotoend" title="Flexible Routing" media="<i class='f7-icons'>forward</i>" />
-      <f7-list-item link="/multiple-languages/" title="Multiple languages" :media="'<img src=\'' + (images['flag_' + $root.language] || images['flag_en']) + '\' width=\'29\' />'" />
-    </f7-list>
-
-    <!-- Link to GitHub repository -->
-    <f7-block style="text-align: center">
-      <f7-link href="http://app-framework.com" external><f7-icon material="open_in_browser" size="18" style="padding-bottom: 3px" />&nbsp;&nbsp;Made with App Framework - {{$root.frameworkVersion}}</f7-link>
-    </f7-block>
-
+   <iframe width="100%" height="50%" src="https://www.youtube.com/embed/0L-6ls2Rrgc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
   </f7-page>
 </template>
 
