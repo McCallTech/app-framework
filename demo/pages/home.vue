a5c8fa640c12ef329fd881f88a56cb4ccf08646b
Andrew Senner <drewdiddy611@gmail.com>
2018-05-20 15:19:38 -0500

    convert iframe to vue-video-player with youtube plugin. (Note: in regards to TDD, this was a spike, still new to Vue TDD)

--- demo/pages/home.vue
+++ demo/pages/home.vue
@@ -12,40 +12,101 @@
     <f7-block inner inset style="text-align: center" v-if="$root.appMode==='mobile'">
       <p><b>For native App feeling, please pin this page to your homescreen and open it from there!</b></p>
     </f7-block>
-   <iframe width="100%" height="50%" src="https://www.youtube.com/embed/0L-6ls2Rrgc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+   <video-player class="video-player-box"
+                 ref="videoPlayer"
+                 :options="playerOptions"
+                 :playsinline="true"
+                 customEventName="customstatechangedeventname"
+
+                 @play="onPlayerPlay($event)"
+                 @pause="onPlayerPause($event)"
+                 @ended="onPlayerEnded($event)"
+                 @statechanged="playerStateChanged($event)"
+                 @ready="playerReadied">
+  </video-player>
   </f7-page>
 </template>
 
 <script>
-  module.exports = {
-    data: function () {
-      return {
-        images: {
-          flag_en: require('../images/flag-en.png'),
-          flag_de: require('../images/flag-de.png'),
-          theme_ios: require('../images/theme-ios.png'),
-          theme_material: require('../images/theme-material.png'),
-          firebase: require('../images/firebase.png')
-        }
+import 'video.js/dist/video-js.min.css';
+import { videoPlayer } from 'vue-video-player';
+import 'videojs-youtube/dist/Youtube.min';
+module.exports = {
+  data: function() {
+    return {
+      playerOptions: {
+        // videojs options
+        muted: true,
+        language: 'en',
+        playbackRates: [0.7, 1.0, 1.5, 2.0],
+        techOrder: ['youtube'],
+        sources: [
+          {
+            type: 'video/youtube',
+            src: 'https://www.youtube.com/embed/0L-6ls2Rrgc'
+          }
+        ]
+      },
+      images: {
+        flag_en: require('../images/flag-en.png'),
+        flag_de: require('../images/flag-de.png'),
+        theme_ios: require('../images/theme-ios.png'),
+        theme_material: require('../images/theme-material.png'),
+        firebase: require('../images/firebase.png')
       }
+    };
+  },
+  components: {
+    videoPlayer
+  },
+  mounted() {
+    console.log('this is current player instance object', this.player);
+  },
+  computed: {
+    player() {
+      return this.$refs.videoPlayer.player;
+    }
+  },
+  methods: {
+    updateSmartlist: function(e) {
+      setTimeout(
+        function() {
+          let text = this.$$(e.target)
+            .find('option[value=' + e.target.value + ']')
+            .text();
+          this.$$(e.target)
+            .parent()
+            .find('.item-after')
+            .html(text);
+        }.bind(this),
+        0
+      );
     },
-    methods: {
-      updateSmartlist: function (e) {
-        setTimeout(function () {
-          let text = this.$$(e.target).find('option[value=' + e.target.value + ']').text()
-          this.$$(e.target).parent().find('.item-after').html(text)
-        }.bind(this), 0)
-      }
+    // listen event
+    onPlayerPlay(player) {
+      // console.log('player play!', player)
+    },
+    onPlayerPause(player) {
+      // console.log('player pause!', player)
+    },
+    // or listen state event
+    playerStateChanged(playerCurrentState) {
+      // console.log('player current update state', playerCurrentState)
+    },
+    // player is ready
+    playerReadied(player) {
+      console.log('the player is readied', player);
     }
   }
+};
 </script>
 
 <style>
-  .f7-icons  {
-    width: 29px;
-    font-size: 29px;
-    height: 29px;
-    text-align: center;
-    color: gray;
-  }
+.f7-icons {
+  width: 29px;
+  font-size: 29px;
+  height: 29px;
+  text-align: center;
+  color: gray;
+}
 </style>
